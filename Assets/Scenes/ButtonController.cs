e5de8a2d89744e43010113923dabf2d24f9e6387
hyotetsu99nmb <hyotetsu99nmb@gmail.com>
2025-06-20 19:56:01 +0900

    [add]シーン移行を追加

--- Assets/Scenes/ButtonController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement; 
-public class ButtonController : MonoBehaviour
-{
-    public void LoadScene(string Mainscene)
-    {
-        SceneManager.LoadScene(Mainscene);
-    }
-}
