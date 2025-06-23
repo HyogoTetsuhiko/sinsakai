d562adc6e9a6a40d3dd91ec7e0793d975f569662
hyotetsu99nmb <hyotetsu99nmb@gmail.com>
2025-06-23 16:54:18 +0900

    [update]敵を踏むとオブジェクト破壊

---
+++ Assets/Script/Enemy_01.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_01 : MonoBehaviour
+{
+    //インスペクターで設定する
+    [Header("移動速度")] public float speed;
+    [Header("重力")] public float gravity;
+    [Header("画面外でも行動するか")] public bool nonVisible;
+
+    //プライベート変数
+    private Rigidbody2D rb = null;
+    private SpriteRenderer sr = null;
+    private ObjectCollision oc = null;
+    private BoxCollider2D col = null;
+    private bool rightTlefF = false;
+    private bool isDead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        oc = GetComponent<ObjectCollision>();
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!oc.playerStepOn)
+        {
+            if (sr.isVisible || nonVisible)
+            {
+                //行動する
+                int xVector = -1;
+                if (rightTlefF)
+                {
+                    xVector = 1;
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                rb.velocity = new Vector2(xVector * speed, -gravity);
+            }
+            else
+            {
+                rb.Sleep();
+            }
+        }
+        //踏まれた時の処理
+        else
+        {
+            rb.velocity = new Vector2(0,-gravity);
+            isDead = true;
+            col.enabled = false;
+            Destroy(gameObject, 3f);
+        }
+    }
+}
