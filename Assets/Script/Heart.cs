96dc9022f33c3d4d56085be1af5517f37a3d11de
hyotetsu99nmb <hyotetsu99nmb@gmail.com>
2025-06-24 16:58:26 +0900

    [add]残機表示追加

---
+++ Assets/Script/Heart.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Heart : MonoBehaviour
+{
+    private　Text heartText = null;
+    private int oldHeartNum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heartText = GetComponent<Text>();
+        if(GameManager.instance != null)
+        {
+            heartText.text = " × " + GameManager.instance.heartNum;
+        }
+        else
+        {
+            Debug.Log("ゲームマネージャー置き忘れ");
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(oldHeartNum != GameManager.instance.heartNum)
+        {
+            heartText.text = " × " + GameManager.instance.heartNum;
+            oldHeartNum = GameManager.instance.heartNum;
+        } 
+    }
+}
